21a212a5ec598d0040ad1e023c5a858a4809116d
alptibet <a.tibet@yandex.ru>
2023-01-09 22:11:36 +0300

    trying new routing scheme

--- src/routes/contractors.rs
+++ src/routes/contractors.rs
@@ -3,20 +3,8 @@ use serde_json::{json, Value};
 
 use crate::appstate::AppState;
 
-// async fn get_contractors(
-//     State(state): State<AppState>,
-// ) -> Result<Json<VecResponse<Contractor>>, Json<AppError>> {
-//     match contractors::find_contractors(state).await {
-//         Ok(_contractor_doc) => Ok(Json(VecResponse {
-//             message: "success".to_string(),
-//             data: _contractor_doc,
-//         })),
-//         Err(_error) => Err(Json(AppError::build(400))),
-//     }
-// }
-
 pub async fn api_hello(State(state): State<AppState>) -> Json<Value> {
-    println!("{:?}", state);
+    println!("{:?}", state.db);
     Json(json!({
         "name":"Alp"
     }))
