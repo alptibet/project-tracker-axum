dde72451a1d3a32bc79eeeaab8e322fd07a4f42f
alptibet <a.tibet@yandex.ru>
2023-04-01 21:35:22 +0300

    removed hardcoded username and pass ))

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Cont from './lib/Cont.svelte';
-  import Counter from './lib/Counter.svelte';
+  import Cont from "./lib/Cont.svelte";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <h1>TEST HEADER</h1>
--- frontend/src/lib/Cont.svelte
+++ frontend/src/lib/Cont.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import axios from 'axios';
+  import axios from "axios";
   const getContractors = async () => {
     try {
       const res = await axios({
-        method: 'get',
-        url: 'http://localhost:3000/api/v1/contractors',
+        method: "get",
+        url: "http://localhost:3000/api/v1/contractors",
         withCredentials: true,
       });
     } catch (error) {
