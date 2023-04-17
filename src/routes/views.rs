1e1e316704a3a01de4bf44a83b009a52ad915d02
alptibet <a.tibet@yandex.ru>
2023-04-17 21:48:11 +0300

    resolving storing user data in state

--- src/routes/views.rs
+++ src/routes/views.rs
@@ -4,14 +4,16 @@ use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse};
 use axum::{routing::get, Router};
 
+pub fn create_view_routes() -> Router<AppState> {
+    Router::new()
+        .route("/", get(render_home))
+        .route("/overview", get(render_overview))
+}
+
 #[derive(Template)]
 #[template(path = "home.html")]
 struct HomeTemplate<'a> {
     title: &'a str,
-}
-
-pub fn create_view_routes() -> Router<AppState> {
-    Router::new().route("/", get(render_home))
 }
 
 pub async fn render_home() -> impl IntoResponse {
@@ -19,6 +21,26 @@ pub async fn render_home() -> impl IntoResponse {
         title: "Project Tracker",
     };
     match home.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(err) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to render template. Error {}", err),
+        )
+            .into_response(),
+    }
+}
+
+#[derive(Template)]
+#[template(path = "overview.html")]
+struct OverViewTemplate<'a> {
+    title: &'a str,
+}
+
+pub async fn render_overview() -> impl IntoResponse {
+    let overview = OverViewTemplate {
+        title: "Project Tracker",
+    };
+    match overview.render() {
         Ok(html) => Html(html).into_response(),
         Err(err) => (
             StatusCode::INTERNAL_SERVER_ERROR,
