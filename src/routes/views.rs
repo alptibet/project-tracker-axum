896f351a2311eeee0a77e408b7fc700e2667072d
alptibet <a.tibet@yandex.ru>
2023-04-17 22:47:06 +0300

    testing templates

--- src/routes/views.rs
+++ src/routes/views.rs
@@ -1,7 +1,5 @@
 use crate::appstate::AppState;
 use askama::Template;
-use axum::http::StatusCode;
-use axum::response::{Html, IntoResponse};
 use axum::{routing::get, Router};
 
 pub fn create_view_routes() -> Router<AppState> {
@@ -12,40 +10,22 @@ pub fn create_view_routes() -> Router<AppState> {
 
 #[derive(Template)]
 #[template(path = "home.html")]
-struct HomeTemplate<'a> {
+pub struct HomeTemplate<'a> {
     title: &'a str,
 }
 
-pub async fn render_home() -> impl IntoResponse {
-    let home = HomeTemplate {
+pub async fn render_home() -> HomeTemplate<'static> {
+    HomeTemplate {
         title: "Project Tracker",
-    };
-    match home.render() {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Failed to render template. Error {}", err),
-        )
-            .into_response(),
     }
 }
 
 #[derive(Template)]
 #[template(path = "overview.html")]
-struct OverViewTemplate<'a> {
+pub struct OverviewTemplate<'a> {
     title: &'a str,
 }
 
-pub async fn render_overview() -> impl IntoResponse {
-    let overview = OverViewTemplate {
-        title: "Project Tracker",
-    };
-    match overview.render() {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Failed to render template. Error {}", err),
-        )
-            .into_response(),
-    }
+pub async fn render_overview() -> OverviewTemplate<'static> {
+    OverviewTemplate { title: "Testing" }
 }
